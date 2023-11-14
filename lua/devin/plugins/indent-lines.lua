d011c909a90bd6eda071a2c18160483ec6b72690
Devin Droddy <devin.droddy@gmail.com>
2023-11-14 10:56:54 -0800

    update indent-lines config to v3

--- lua/devin/plugins/indent-lines.lua
+++ lua/devin/plugins/indent-lines.lua
@@ -1,13 +1,14 @@
 return {
-	'lukas-reineke/indent-blankline.nvim', config = function()
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		space_char_blankline = " ",
+		show_current_context = true,
+		show_current_context_start = true,
+	},
+	config = function()
 		vim.opt.list = true
-		vim.opt.listchars:append "space:⋅"
-		vim.opt.listchars:append "eol:↴"
-
-		require("indent_blankline").setup {
-			space_char_blankline = " ",
-			show_current_context = true,
-			show_current_context_start = true,
-		}
-	end
+		vim.opt.listchars:append("space:⋅")
+		vim.opt.listchars:append("eol:↴")
+	end,
 }
