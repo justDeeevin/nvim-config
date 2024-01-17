8eac5ce3f3288553eb3700dccc0d21a72d85677e
Devin Droddy <devin.droddy@gmail.com>
2024-01-17 13:12:43 -0500

    update to harpoon 2

--- lua/devin/plugins/harpoon.lua
+++ lua/devin/plugins/harpoon.lua
@@ -1,8 +1,12 @@
-return {'ThePrimeAgen/harpoon', config = function()
-local mark = require('harpoon.mark')
-local ui = require('harpoon.ui')
-
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-end
+return {
+	"ThePrimeAgen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("harpoon").setup()
+		local mark = require("harpoon.mark")
+		local ui = require("harpoon.ui")
+		vim.keymap.set("n", "<leader>a", mark.add_file)
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+	end,
 }
