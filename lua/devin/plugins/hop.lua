553023fdd25d86eeb69454360d59b6d3a725ba17
Devin Droddy <devin.droddy@gmail.com>
2024-03-07 11:53:44 -0500

    Add colemak config conditional to env variable COLEMAK

--- lua/devin/plugins/hop.lua
+++ lua/devin/plugins/hop.lua
@@ -1,13 +1,23 @@
-return {'phaazon/hop.nvim', config = function()
-	local hop = require('hop')
-	hop.setup()
-	local directions = require('hop.hint').HintDirection
-	vim.keymap.set('', '<leader>l', function()
-		hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-	end, { remap = true })
-	vim.keymap.set('', '<leader>h', function()
-		hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-	end, { remap = true })
-	vim.keymap.set('', '<leader>w', vim.cmd.HopWordCurrentLine)
-end
+return {
+	"phaazon/hop.nvim",
+	config = function()
+		local hop = require("hop")
+		hop.setup()
+		local directions = require("hop.hint").HintDirection
+		if os.getenv("COLEMAK") == "1" then
+			vim.keymap.set("", "<leader>i", function()
+				hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+			end, { remap = true })
+			vim.keymap.set("", "<leader>h", function()
+				hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+			end, { remap = true })
+		else
+			vim.keymap.set("", "<leader>l", function()
+				hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+			end, { remap = true })
+			vim.keymap.set("", "<leader>h", function()
+				hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+			end, { remap = true })
+		end
+	end,
 }
--- lua/devin/remaps.lua
+++ lua/devin/remaps.lua
@@ -1,14 +1,26 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("i", "kj", "<Esc>")
-
--- Move selected lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- Keep search results in the center of the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+if os.getenv("COLEMAK") == "1" then
+	vim.keymap.set("i", "en", "<Esc>")
+	vim.keymap.set("n", "s", "<Cmd>startinsert<CR>")
+	vim.keymap.set({ "n", "o", "x" }, "n", "<Down>")
+	vim.keymap.set({ "n", "o", "x" }, "e", "<Up>")
+	vim.keymap.set({ "n", "o", "x" }, "i", "<Right>")
+	vim.keymap.set({ "n", "o", "x" }, "k", function()
+		vim.fn.search(vim.fn.histget("search", -1))
+	end)
+	vim.keymap.set({ "n", "o", "x" }, "K", function()
+		vim.fn.search(vim.fn.histget("search", -1), "b")
+	end)
+	-- Move selected lines
+	vim.keymap.set("v", "N", ":m '>+1<CR>gv=gv")
+	vim.keymap.set("v", "E", ":m '<-2<CR>gv=gv")
+else
+	vim.keymap.set("i", "jk", "<Esc>")
+	-- Move selected lines
+	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+end
 
 vim.keymap.set("n", "<C-a>", function()
 	vim.lsp.buf.code_action()
