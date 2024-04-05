f0c32d3ce35b23a0401bcd77554790839015d229
Devin Droddy <devin.droddy@gmail.com>
2024-04-05 13:45:42 -0400

    add tsserver inlay hint config

--- lua/devin/plugins/lsp.lua
+++ lua/devin/plugins/lsp.lua
@@ -171,6 +171,34 @@ return {
 					},
 				},
 			},
+			tsserver = {
+				  settings = {
+                                          javascript = {
+                                            inlayHints = {
+                                              includeInlayEnumMemberValueHints = true,
+                                              includeInlayFunctionLikeReturnTypeHints = true,
+                                              includeInlayFunctionParameterTypeHints = true,
+                                              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                                              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                                              includeInlayPropertyDeclarationTypeHints = true,
+                                              includeInlayVariableTypeHints = false,
+                                            },
+                                          },
+
+                                          typescript = {
+                                            inlayHints = {
+                                              includeInlayEnumMemberValueHints = true,
+                                              includeInlayFunctionLikeReturnTypeHints = true,
+                                              includeInlayFunctionParameterTypeHints = true,
+                                              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                                              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                                              includeInlayPropertyDeclarationTypeHints = true,
+                                              includeInlayVariableTypeHints = false,
+                                            },
+                                          },
+				  }
+
+			}
 		}
 
 		-- Ensure the servers and tools above are installed
