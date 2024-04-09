1da945e61d03e9b6429f568b95bbdcb3946a698f
Devin Droddy <devin.droddy@gmail.com>
2024-04-08 20:45:29 -0400

    colemak-dh change

--- lua/devin/remaps.lua
+++ lua/devin/remaps.lua
@@ -6,8 +6,10 @@ if os.getenv("COLEMAK") == "1" then
 	vim.keymap.set({ "n", "o", "x" }, "n", "j")
 	vim.keymap.set({ "n", "o", "x" }, "e", "k")
 	vim.keymap.set({ "n", "o", "x" }, "i", "l")
+	vim.keymap.set({ "n", "o", "x" }, "m", "h")
 	vim.keymap.set({ "n", "o", "x" }, "k", "n")
 	vim.keymap.set({ "n", "o", "x" }, "K", "N")
+	vim.keymap.set({ "n", "o", "x" }, "h", "m")
 	-- Move selected lines
 	vim.keymap.set("v", "N", ":m '>+1<CR>gv=gv")
 	vim.keymap.set("v", "E", ":m '<-2<CR>gv=gv")
