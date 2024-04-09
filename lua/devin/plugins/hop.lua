d8b963314d899a86e7f78139f7eb15490c4e0c5f
Devin Droddy <devin.droddy@gmail.com>
2024-04-09 17:44:35 -0400

    switch colemak hop keybinds to mod-dh

--- lua/devin/plugins/hop.lua
+++ lua/devin/plugins/hop.lua
@@ -8,7 +8,7 @@ return {
 			vim.keymap.set("", "<leader>i", function()
 				hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 			end, { remap = true, desc = "Hop to a character after the cursor on the current line" })
-			vim.keymap.set("", "<leader>h", function()
+			vim.keymap.set("", "<leader>m", function()
 				hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 			end, { remap = true, desc = "Hop to a character before the cursor on the current line" })
 		else
