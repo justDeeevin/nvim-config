66251237d03ad4154ba41d93b13a01c3cd0f8765
Devin Droddy <devin.droddy@gmail.com>
2024-08-02 17:42:57 -0400

    remove codewindow

--- lua/devin/plugins/oil.lua
+++ lua/devin/plugins/oil.lua
@@ -1,1 +1,8 @@
-return { "stevearc/oil.nvim", opts = { view_options = { show_hidden = true } } }
+return {
+	"stevearc/oil.nvim",
+	config = function()
+		local oil = require("oil")
+		oil.setup({ view_options = { show_hidden = true } })
+		vim.keymap.set("n", "<leader>e", oil.open, { desc = "Open Oil" })
+	end,
+}
