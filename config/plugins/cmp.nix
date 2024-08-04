f4d13ff47ab388d4b2fe87569ec610ee5eea931c
Devin Droddy <devin.droddy@gmail.com>
2024-08-03 23:54:20 -0400

    fix up in cmp list

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -3,39 +3,39 @@
     enable = true;
     autoEnableSources = true;
     settings = {
-      sources =[
-        {name = "nvim_lsp";}
-        {name = "path";}
-	{name = "luasnip";}
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "luasnip"; }
       ];
       # This is the default with noselect removed. This change makes it select the first item automatically.
       completion.completeopt = "menu, menuone";
       mapping = {
         "<CR>" = "cmp.mapping.confirm({select = true})";
-	"<C-Space>" = ''
-          function()
-            cmp.mapping.complete()
-          end
-	'';
-	"<Down>" = ''
-	  function(fallback)
-	    if cmp.visible() then
-	      cmp.select_next_item()
-	    else
-	      fallback()
-	    end
-	  end
-	'';
-	"<Up>" = ''
-	  function(fallback)
-	    if cmp.visible() then
-	      cmp.select_next_item()
-	    else
-	      fallback()
-	    end
-	  end
-	'';
+        "<C-Space>" = ''
+                    function()
+                      cmp.mapping.complete()
+                    end
+          	'';
+        "<Down>" = ''
+          	  function(fallback)
+          	    if cmp.visible() then
+          	      cmp.select_next_item()
+          	    else
+          	      fallback()
+          	    end
+          	  end
+          	'';
+        "<Up>" = ''
+          	  function(fallback)
+          	    if cmp.visible() then
+          	      cmp.select_prev_item()
+          	    else
+          	      fallback()
+          	    end
+          	  end
+          	'';
       };
     };
   };
-  }
+}
