2138276a8d05e812eb61b193294e5fa4f870df5e
Devin Droddy <devin.droddy@gmail.com>
2024-08-08 19:07:14 -0400

    update cmp mappings

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -11,30 +11,46 @@
       # This is the default with noselect removed. This change makes it select the first item automatically.
       completion.completeopt = "menu, menuone";
       mapping = {
-        "<CR>" = "cmp.mapping.confirm({select = true})";
-        "<C-Space>" = ''
-                    function()
-                      cmp.mapping.complete()
-                    end
-          	'';
+        "<CR>" = "require('cmp').mapping.confirm({select = true})";
+        "<C-@>" = ''
+          function()
+            require("cmp").mapping.complete()
+          end
+        '';
         "<Down>" = ''
-          	  function(fallback)
-          	    if cmp.visible() then
-          	      cmp.select_next_item()
-          	    else
-          	      fallback()
-          	    end
-          	  end
-          	'';
+          function(fallback)
+            if require("cmp").visible() then
+              require("cmp").select_next_item()
+            else
+              fallback()
+            end
+          end
+        '';
         "<Up>" = ''
-          	  function(fallback)
-          	    if cmp.visible() then
-          	      cmp.select_prev_item()
-          	    else
-          	      fallback()
-          	    end
-          	  end
-          	'';
+          function(fallback)
+            if require("cmp").visible() then
+              require("cmp").select_prev_item()
+            else
+              fallback()
+            end
+          end
+        '';
+        "<C-i>" = ''
+          require("cmp").mapping(function()
+            local luasnip = require("luasnip")
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, {"i", "s"})
+        '';
+        "<C-m>" = ''
+          require("cmp").mapping(function()
+            local luasnip = require("luasnip")
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, {"i", "s"})
+        '';
       };
     };
   };
