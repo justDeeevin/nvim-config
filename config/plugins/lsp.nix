a92380ea4fd90ea47e4d1850c9a69d5fdd0668cc
Devin Droddy <devin.droddy@gmail.com>
2024-08-08 19:07:06 -0400

    add item highlighting

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -18,4 +18,42 @@
       action = "<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))<CR>";
     }
   ];
+
+  autoGroups = {
+    lsp-highlight = {
+      clear = false;
+    };
+    lsp-detach = {
+      clear = true;
+    };
+  };
+
+  autoCmd = [
+    {
+      event = [ "CursorHold" "CursorHoldI" ];
+      group = "lsp-highlight";
+      callback = {
+        __raw = "vim.lsp.buf.document_highlight";
+      };
+    }
+    {
+      event = [ "CursorMoved" "CursorMovedI" ];
+      group = "lsp-highlight";
+      callback = {
+        __raw = "vim.lsp.buf.clear_references";
+      };
+    }
+    {
+      event = "LspDetach";
+      group = "lsp-detach";
+      callback = {
+        __raw = ''
+          function(event)
+            vim.lsp.buf.clear_references()
+            vim.api.nvim_clear_autocmds({group = "lsp-highlight"})
+          end
+        '';
+      };
+    }
+  ];
 }
