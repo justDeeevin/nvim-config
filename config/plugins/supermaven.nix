49ca02735ed152f309e3546ab48aadcfbb220312
Devin Droddy <devin.droddy@gmail.com>
2024-09-09 09:30:14 -0400

    formatting

--- config/plugins/supermaven.nix
+++ config/plugins/supermaven.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   extraPlugins = with pkgs.vimPlugins; [
     supermaven-nvim
   ];
 
   autoCmd = [
     {
-      # On startup, silently etup supermaven
+      # On startup, silently setup supermaven
       event = "VimEnter";
       command = "lua require('supermaven-nvim').setup({})";
       once = true;
