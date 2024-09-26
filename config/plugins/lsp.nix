286abc38a702b84c7e02c5bccf4a39781acc699e
Devin Droddy <devin.droddy@gmail.com>
2024-09-26 16:09:24 -0400

    only attach lsp-highlight autocmds if the current lsp supports it

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -61,21 +61,34 @@
     lsp-detach = {
       clear = true;
     };
+    lsp-attach = {
+      clear = true;
+    };
   };
 
   autoCmd = [
     {
-      event = ["CursorHold" "CursorHoldI"];
-      group = "lsp-highlight";
+      event = "LspAttach";
+      group = "lsp-attach";
       callback = {
-        __raw = "vim.lsp.buf.document_highlight";
-      };
-    }
-    {
-      event = ["CursorMoved" "CursorMovedI"];
-      group = "lsp-highlight";
-      callback = {
-        __raw = "vim.lsp.buf.clear_references";
+        __raw = ''
+          function(event)
+            local client = vim.lsp.get_client_by_id(event.data.client_id)
+            if client and client.server_capabilities.documentHighlightProvider then
+              vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                buffer = event.buf,
+                group = "lsp-highlight",
+                callback = vim.lsp.buf.document_highlight,
+              })
+
+              vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                buffer = event.buf,
+                group = "lsp-highlight",
+                callback = vim.lsp.buf.clear_references,
+              })
+            end
+          end
+        '';
       };
     }
     {
