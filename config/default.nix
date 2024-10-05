f8dde8e179fdfb56fd492b2b5799c0d20c919558
Devin Droddy <devin.droddy@gmail.com>
2024-10-05 00:47:18 -0400

    switch to oxocarbon

--- config/default.nix
+++ config/default.nix
@@ -1,7 +1,6 @@
 {
   # Import all your configuration modules here
-  imports = [ ./remaps.nix ./opts.nix ] ++ builtins.map (file: ./. + "/plugins/${file}") (builtins.attrNames (builtins.readDir ./plugins));
+  imports = [./remaps.nix ./opts.nix] ++ builtins.map (file: ./. + "/plugins/${file}") (builtins.attrNames (builtins.readDir ./plugins));
 
-  colorschemes.cyberdream.enable = true;
-  colorschemes.cyberdream.settings.transparent = true;
+  colorschemes.oxocarbon.enable = true;
 }
