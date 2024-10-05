26937726057869d002c0c3b7142904b3eee6edf5
Devin Droddy <devin.droddy@gmail.com>
2024-10-04 21:55:41 -0400

    swap codeshot for codesnap

---
+++ config/plugins/codesnap.nix
@@ -0,0 +1,11 @@
+{
+  plugins.codesnap = {
+    enable = true;
+    settings = {
+      watermark = "";
+      has_breadcrumbs = true;
+      has_line_number = true;
+      background_theme = "sea";
+    };
+  };
+}
