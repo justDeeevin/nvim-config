92b9331af5192ced2621f4da5562e141c5c45914
Devin Droddy <devin.droddy@gmail.com>
2024-10-24 14:07:46 -0400

    better oil config

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -1,7 +1,16 @@
 {
   plugins.oil = {
     enable = true;
-    settings.view_options.show_hidden = true;
+    settings = {
+      view_options = {
+        show_hidden = true;
+        is_always_hidden = {
+          __raw = "function(name, bufnr) return name == '.git' end";
+        };
+      };
+      skip_confirm_for_simple_edits = true;
+      watch_for_changes = true;
+    };
   };
 
   keymaps = [
