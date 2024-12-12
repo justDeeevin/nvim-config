f1f793093429087ca65015a556f617c3f36ee55b
Devin Droddy <devin.droddy@gmail.com>
2024-12-12 14:09:07 -0500

    add `:Wnf` for saving without formatting

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -22,8 +22,25 @@
         tex = ["latexindent"];
         rust = ["rustfmt"];
       };
-      format_on_save.timeout_ms = 1000;
+      format_on_save.__raw = ''
+        function(bufnr)
+          if vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 1000 }
+        end
+      '';
     };
+  };
+
+  userCommands.Wnf = {
+    command.__raw = ''
+      function(args)
+        vim.b.disable_autoformat = true
+        vim.api.nvim_command("write")
+        vim.b.disable_autoformat = false
+      end
+    '';
   };
 
   extraPackages = with pkgs; [
